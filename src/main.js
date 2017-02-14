34d8bc363092165fd9b0239260a524d8e5809b20
Andrew Kud <kud@mm.st>
2017-02-14 08:41:51 +0200

    render App

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('foo')
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
+
+const container = document.querySelector(".container");
+
+ReactDom.render(<App />, container);
+
+if (module.hot) {
+    module.hot.accept();
+}
