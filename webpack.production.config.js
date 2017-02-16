e5640dc0713cd2d10270b4eb21420405fb3cfe91
Andrew Kud <kud@mm.st>
2017-02-16 09:22:03 +0200

    add html plugins

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,5 +1,6 @@
 var path = require("path");
 var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 var config = {
     context: path.join(__dirname, "src"),
@@ -22,6 +23,10 @@ var config = {
                 test: /\.sass$/,
                 use: ["style-loader", "css-loader", "sass-loader"],
             },
+            {
+                test: /\.pug$/,
+                use: ["pug-html-loader?pretty"],
+            },
         ],
     },
     resolve: {
@@ -38,6 +43,11 @@ var config = {
           }
         }),
         new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "views/index.pug",
+            title: "Tic Tac Toe"
+        }),
     ]
 };
 
