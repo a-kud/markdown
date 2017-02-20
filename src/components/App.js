8b43f87f897c9e63c9d7576ec5a3c0c5878fac5c
Andrew Kud <kud@mm.st>
2017-02-20 10:41:44 +0200

    add sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
 import React from "react";
+import marked from "marked"
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            value: "",
         };
+
+        this.onTextareaChange = this.onTextareaChange.bind(this);
+    }
+
+    onTextareaChange(e) {
+        this.setState({
+            value: e.target.value,
+        })
     }
 
     render() {
         return (
             <div className="app">
-                <label>Enter markdown here:
-                    <input type="text"></input>
+                <label id="markdown">Enter markdown here:
+                    <textarea value={this.state.value} onChange={this.onTextareaChange}></textarea>
                 </label>
+                <div id="output" dangerouslySetInnerHTML={{__html: marked(this.state.value)}}></div>
             </div>
         );
     }
