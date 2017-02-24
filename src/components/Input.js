45f4e607ee8d36d72e90158d3435b777cfb99ca2
Andrew Kud <kud@mm.st>
2017-02-24 08:12:00 +0200

    refactor

---
+++ src/components/Input.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class InputMarkdown extends React.Component {
+    render() {
+        return (
+            <label id="markdown">Enter markdown here:
+                <textarea value={this.props.value} onChange={(e) => this.props.onChange(e)}></textarea>
+            </label>
+        );
+    }
+}
+
+export default InputMarkdown;
