8c1c23929f8e9da4b1ec0d083c1212b784d74d2a
Andrew Kud <kud@mm.st>
2017-02-27 09:06:11 +0200

    components refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import marked from "marked"
 import InputMarkdown from "./Input";
+import RenderMarkdown from "./Output";
 
 class App extends React.Component {
     constructor(props) {
@@ -22,7 +22,7 @@ class App extends React.Component {
         return (
             <div className="app">
                 <InputMarkdown value={this.state.value} onChange={this.handleTextareaChange}/>
-                <div id="output" dangerouslySetInnerHTML={{__html: marked(this.state.value)}}></div>
+                <RenderMarkdown value={this.state.value} />
             </div>
         );
     }
